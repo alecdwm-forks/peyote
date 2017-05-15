5a7cfa8e7e9352bd68c7f76925c85ad9f29e0f46
Armin <armin@acid>
2017-05-15 14:43:21 +0200

    add return code check and powerline segment (gets return code via first argument)

--- peyote.go
+++ peyote.go
@@ -5,10 +5,12 @@ import "flag"
 import "log"
 import "os"
 import "strings"
+import "strconv"
 
 func main(){
+  returncode := os.Args[1]
   username := os.Getenv("USER")
-  hostname := "z"
+  hostname := "acid"
   dir, err := os.Getwd()
   homedir := os.Getenv("HOME")
   dirname := strings.Replace(dir, homedir, "~", 1)
@@ -17,7 +19,15 @@ func main(){
 
   }
   flag.Parse()
-  fmt.Printf("%%F{254}%%K{23} %s@%s %%K{241}%%F{23}%%F{254}%%K{241} %s %%f%%k%%F{241}%%f%%k ", username, hostname, dirname)
+  returncodeint, err := strconv.Atoi(returncode)
+  if err != nil {
+    log.Fatal(err)
+  }
+  if (returncodeint > 0) {
+    fmt.Printf("%%F{254}%%K{23} %s@%s %%K{241}%%F{23}%%F{254}%%K{241} %s %%K{236}%%F{241}%%F{203}%%K{236} %s %%f%%k%%F{236} %%f%%k", username, hostname, dirname, returncode)
+  } else {
+    fmt.Printf("%%F{254}%%K{23} %s@%s %%K{241}%%F{23}%%F{254}%%K{241} %s %%f%%k%%F{241}%%f%%k ", username, hostname, dirname)
+  }
 }
 
 
