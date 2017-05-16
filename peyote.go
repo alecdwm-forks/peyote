3ca54dd6229d9cd6eda67ea4bd5b6149e48cfc98
Armin <armin@acid>
2017-05-16 13:44:40 +0200

    get usercolor from second commandline argument NOTE: this requires that peyote will be called with a second argument from within zshrc ($EUID as present in the shell env)

--- peyote.go
+++ peyote.go
@@ -10,6 +10,17 @@ import "strconv"
 func main(){
   returncode := os.Args[1]
   username := os.Getenv("USER")
+  euid := os.Args[2]
+  euidnum, err := strconv.Atoi(euid)
+  if err != nil {
+    log.Fatal(err)
+  }
+  usercolor := "1"
+  if euidnum > 0 {
+    usercolor = "23"
+  } else {
+    usercolor = "88"
+  }
   hostname, err := os.Hostname()
   dir, err := os.Getwd()
   homedir := os.Getenv("HOME")
@@ -24,9 +35,9 @@ func main(){
     log.Fatal(err)
   }
   if (returncodeint > 0) {
-    fmt.Printf("%%F{254}%%K{23} %s@%s %%K{241}%%F{23}%%F{254}%%K{241} %s %%K{236}%%F{241}%%F{203}%%K{236} %s %%f%%k%%F{236} %%f%%k", username, hostname, dirname, returncode)
+    fmt.Printf("%%F{254}%%K{%s} %s@%s %%K{241}%%F{%s}%%F{254}%%K{241} %s %%K{236}%%F{241}%%F{203}%%K{236} %s %%f%%k%%F{236} %%f%%k", usercolor, username, hostname, usercolor, dirname, returncode)
   } else {
-    fmt.Printf("%%F{254}%%K{23} %s@%s %%K{241}%%F{23}%%F{254}%%K{241} %s %%f%%k%%F{241}%%f%%k ", username, hostname, dirname)
+    fmt.Printf("%%F{254}%%K{%s} %s@%s %%K{241}%%F{%s}%%F{254}%%K{241} %s %%f%%k%%F{241}%%f%%k ", usercolor, username, hostname, usercolor, dirname)
   }
 }
 
